b98ee13d7a1e74839cd2966ec1cb1f54db3aef33
Jon Smith <jon.smith@crick.ac.uk>
2019-02-04 13:57:08 +0000

    different version of fiji

--- runnables/RunLocalisation.sh
+++ runnables/RunLocalisation.sh
@@ -54,7 +54,7 @@ case ${i} in
 esac
 done
 
-module load Fiji/1.52d-foss-2016b
+module load Fiji/1.51
 module load Tigervnc/1.9.0
 #module load X11/20160819-foss-2016b
 export DISPLAY=`vncserver 2>&1 | grep -oP '(?<=desktop is ).*'`
