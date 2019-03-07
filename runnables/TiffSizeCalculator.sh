9083c0668616887234eb7f92b0755ec44681252b
Jon Smith <jon.smith@crick.ac.uk>
2019-03-07 14:05:39 +0000

    better tiff size calculation

--- runnables/TiffSizeCalculator.sh
+++ runnables/TiffSizeCalculator.sh
@@ -25,13 +25,16 @@ done
 ml LibTIFF/4.0.4-foss-2016b
 
 # No images
-count = 0
+count=0
 
 # Identify the tiff stack size
 OLD_IFS=${IFS}
 IFS=","
 for file in ${FILES}; do
-    count+=$(tiffinfo ${file} | grep Frame | sed 's/^.*"Frame":// ; s/,".*$//' | sort -rn | head -n 1)
+    echo "Counting tiff length for file: ${file}"
+    current=$(tiffinfo ${file} | grep Frame | sed 's/^.*"Frame":// ; s/,".*$//' | sort -rn | head -n 1)
+    echo "Found: ${current}"
+    count=$((${count} + ${current}))
 done
 export IFS=${OLD_IFS}
 
